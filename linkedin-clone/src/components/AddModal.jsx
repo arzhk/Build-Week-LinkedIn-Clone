664c9d16bc7e334b06432ea10b75b13d85993ef8
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2020-12-01 10:54:40 +0100

    start

---
+++ linkedin-clone/src/components/AddModal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+class AddModal extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+    }
+
+    render() {
+        return <div></div>;
+    }
+}
+
+
+export default AddModal;
--- linkedin-clone/src/components/Experience.jsx
+++ linkedin-clone/src/components/Experience.jsx
@@ -1,67 +1,28 @@
 import React, { Component } from "react";
 import { Container, ListGroup } from "react-bootstrap";
+import ExperienceItem from "./ExperienceItem";
 
 class Experince extends Component {
+  state = {
+    editShow: false,
+    addShow: false,
+    experiences: [],
+  }
+  addModal = () => { }
+  editModal = () => { }
+  getExperienceFetcher = () => { }
+  addExperiencePost = () => { }
+  editExperiencePut = () => { }
   render() {
     return (
       <Container>
         <div id="experience-main-container" className="experience-contain mb-0">
           <div className="d-flex align-items-center justify-content-between">
             <h4 className="font-weight-normal">Experience</h4>
-            <i className="fas fa-plus mr-3 "></i>
+            <a onClick={this.addModal}><i className="fas fa-plus mr-3 "></i></a>
           </div>
           <ListGroup>
-            <ListGroup.Item variant="light" className="d-flex align-items-center justify-content-between">
-              <div className="d-flex align-items-start">
-                <div className="imgPlace mr-4"></div>
-                <div className="d-flex flex-column">
-                  <h5 className="mb-0 font-weight-normal">Experience 1</h5>
-                  <p className="mb-0 ">Subtext</p>
-                  <p className="mb-0 font-weight-light">Subtext 2</p>
-                  <small className="font-weight-light">Location</small>
-                </div>
-              </div>
-              <i className="fas fa-pen "></i>
-            </ListGroup.Item>
-            <ListGroup.Item variant="light" className="d-flex align-items-center justify-content-between">
-              <div className="d-flex align-items-start">
-                <div className="imgPlace mr-4"></div>
-                <div className="d-flex flex-column">
-                  <h5 className="mb-0 font-weight-normal">Experience 1</h5>
-                  <p className="mb-0 ">Subtext</p>
-                  <p className="mb-0 font-weight-light">Subtext 2</p>
-                  <small className="font-weight-light">Location</small>
-                </div>
-              </div>
-              <i className="fas fa-pen "></i>
-            </ListGroup.Item>
-            <ListGroup.Item variant="light" className="d-flex align-items-center justify-content-between">
-              <div className="d-flex align-items-start">
-                <div className="imgPlace mr-4"></div>
-                <div className="d-flex flex-column">
-                  <h5 className="mb-0 font-weight-normal">Experience 1</h5>
-                  <p className="mb-0 ">Subtext</p>
-                  <p className="mb-0 font-weight-light">Subtext 2</p>
-                  <small className="font-weight-light">Location</small>
-                </div>
-              </div>
-              <i className="fas fa-pen "></i>
-            </ListGroup.Item>
-            <ListGroup.Item
-              variant="light"
-              className="d-flex no-border align-items-center justify-content-between mb-0"
-            >
-              <div className="d-flex align-items-start">
-                <div className="imgPlace mr-4"></div>
-                <div className="d-flex flex-column">
-                  <h5 className="mb-0 font-weight-normal">Experience 1</h5>
-                  <p className="mb-0 ">Subtext</p>
-                  <p className="mb-0 font-weight-light">Subtext 2</p>
-                  <small className="font-weight-light">Location</small>
-                </div>
-              </div>
-              <i className="fas fa-pen "></i>
-            </ListGroup.Item>
+            {this.state.experiences.map((exp) => <ExperienceItem experience={exp} editModal={this.editModal} />)}
           </ListGroup>
         </div>
       </Container>
