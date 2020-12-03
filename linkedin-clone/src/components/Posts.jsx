35717d50320f4b664f26ad9fbd9b9be8d79e6c59
Vanessa Cattabiani <vanebrunocattabiani@gmail.com>
2020-12-03 23:29:02 +0100

    Fetch, CRUD api for reactions

--- linkedin-clone/src/components/Posts.jsx
+++ linkedin-clone/src/components/Posts.jsx
@@ -39,18 +39,115 @@ const Posts = (props) => {
 
     /*****************REACTIONS************************/
     /******STATES FOR REACTION*******/
-    let [reaction, setReaction] = React.useState("0");
+    let [reaction, setReaction] = React.useState("1");
+    let [AllComments, setAllComments] = React.useState([]);
+    let [AllReactions, setAllReactions] = React.useState([]);
     let [icon, setIcon] = React.useState(faThumbsUp);
     let [color, setColor] = React.useState("gray");
     let [word, setWord] = React.useState("Like");
+    let [numberLikes, setNumberLikes] = React.useState(0);
+    let [numberComments, setNumberComments] = React.useState(0);
+    let [liked, setLiked] = React.useState([false, ""])
     /******FUNCTIONS FOR REACTION*******/
-    const getReactions = async () => { }
+    const url = "https://striveschool-api.herokuapp.com/api/comments/"
 
-    const postReactions = async () => { }
+    const getReactions = async () => {
+        console.log("get")
+        try {
+            let response = await fetch(url + _id, {
+                headers: new Headers({
+                    Authorization:
+                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmM0YzQ4ZmVkMjY2ODAwMTcwZWEzZDgiLCJpYXQiOjE2MDY3MzA4OTUsImV4cCI6MTYwNzk0MDQ5NX0.Qzj6OQCKSyxDgEgIadVbBI70XPPAgDlcGoWJEKyM6cU",
+                }),
+            });
+            if (response.ok) {
+
+                setAllComments(await response.json());
+                setAllReactions(AllComments.filter((comment) => comment.comment === '/reactions'));
+                setNumberComments(AllComments.length - AllReactions.length);
+                setNumberLikes(AllReactions.length);
+                console.log(AllComments, AllReactions, numberComments, numberLikes)
+            } else {
+                console.log(response)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    const postReactions = async (rate) => {
+        console.log("post")
+        let commentReaction = {
+            elementId: _id,
+            rate: rate,
+            comment: '/reactions',
+        }
+        try {
+            let response = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(commentReaction),
+                headers: new Headers({
+                    "Content-Type": "application/json",
+                    Authorization:
+                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmM0YzQ4ZmVkMjY2ODAwMTcwZWEzZDgiLCJpYXQiOjE2MDY3MzA4OTUsImV4cCI6MTYwNzk0MDQ5NX0.Qzj6OQCKSyxDgEgIadVbBI70XPPAgDlcGoWJEKyM6cU",
+                }),
+            });
+            if (response.ok) {
+                console.log(await response.json())
+                getReactions();
+            } else {
+                console.log(response)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    const putReactions = async (rate, id) => {
+        console.log("put")
+        let commentReaction = {
+            elementId: _id,
+            rate: rate,
+            comment: '/reactions',
+        }
+        try {
+            let response = await fetch(url + id, {
+                method: 'put',
+                body: JSON.stringify(commentReaction),
+                headers: new Headers({
+                    "Content-Type": "application/json",
+                    Authorization:
+                        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmM0YzQ4ZmVkMjY2ODAwMTcwZWEzZDgiLCJpYXQiOjE2MDY3MzA4OTUsImV4cCI6MTYwNzk0MDQ5NX0.Qzj6OQCKSyxDgEgIadVbBI70XPPAgDlcGoWJEKyM6cU",
+                }),
+            });
+            if (response.ok) {
+                console.log(await response.json())
+            } else {
+                console.log(response)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+    const handleReactions = () => {
+        console.log("handel")
+        let userReaction = AllReactions.filter((reaction) => reaction.elementId === _id);
+        if (userReaction.length > 0) {
+            setLiked([true, userReaction[0]._id])
+        }
+    }
+
+
 
     const handleChange = (rate) => {
+        handleReactions();
         setReaction(rate);
         likeButton(rate);
+        console.log(liked, numberLikes)
+        liked[0] ? putReactions(rate, liked[1]) : postReactions(rate)
     }
 
     const likeButton = (rate) => {
@@ -58,27 +155,27 @@ const Posts = (props) => {
         switch (rate) {
             case 1:
                 setIcon(faThumbsUp)
-                setColor("blue")
+                setColor("#5894f5")
                 setWord("Like")
                 break;
             case 2:
                 setIcon(faSignLanguage)
-                setColor("green")
+                setColor("#40ba32")
                 setWord("Celebrate")
                 break;
             case 3:
                 setIcon(faHandHoldingHeart)
-                setColor("pink")
+                setColor("#8f4f9c")
                 setWord("Support")
                 break;
             case 4:
                 setIcon(faHeart)
-                setColor("red")
+                setColor("#b81d1d")
                 setWord("Love")
                 break;
             case 5:
                 setIcon(faLightbulb)
-                setColor("yellow")
+                setColor("#dbcd00")
                 setWord("Insightful")
                 break;
 
@@ -147,16 +244,18 @@ const Posts = (props) => {
                 </Col>
             </Row>
 
-            <Card.Body className="d-flex justify-content-between border-bottom mb-2 ">
+            <Card.Body className="d-flex justify-content-between mb-2 ">
                 <Col>
                     <Row><p>{text}</p></Row>
                     <Row>{props.data.image !== undefined && props.data.image !== null && !props.data.image.startsWith("file") && !props.data.image.startsWith("blob") && <Image src={props.data.image} fluid />}</Row>
                 </Col>
             </Card.Body>
+            <p className="border-bottom mb-2 "> {numberLikes > 0 && <><FontAwesomeIcon icon={faThumbsUp} style={{ color: "5894f5" }} /> {numberLikes}</>}{numberComments > 0 && <> - {numberComments} Comments</>} </p>
+
             <Row className="mx-2 reactions d-flex justify-content-around">
                 <Button variant="light" className="likeBtn" onClick={() => handleChange(1)} style={{ color: color }} ><FontAwesomeIcon
                     icon={icon} /><span className="d-sm-none d-lg-inline-block"> {word}</span> </Button>
-                <Button variant="light" onClick={toggleComments}><FontAwesomeIcon icon={faCommentDots} /><pan className="d-sm-none d-lg-inline-block">Comment</pan> </Button>
+                <Button variant="light" onClick={toggleComments}><FontAwesomeIcon icon={faCommentDots} /><span className="d-sm-none d-lg-inline-block">Comment</span> </Button>
                 <Button variant="light"><FontAwesomeIcon icon={faShare} /> <span className="d-sm-none d-lg-inline-block">Share</span> </Button>
                 <Button variant="light"><FontAwesomeIcon icon={faPaperPlane} /> <span className="d-sm-none d-lg-inline-block">Send</span></Button>
                 <Row className="like-choice" >
