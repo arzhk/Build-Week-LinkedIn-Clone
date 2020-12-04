714bf7dd8a3e586b7988ddb344cedc3d180899a1
Khojiakbarkhon <isakov56cc@gmail.com>
2020-12-04 16:17:57 +0100

    MyNetwork
    Loaders
    Show more and see all button
    online events
    side bar

--- linkedin-clone/src/components/loaders/InvitationsLoader.jsx
+++ linkedin-clone/src/components/loaders/InvitationsLoader.jsx
@@ -4,19 +4,19 @@ import ContentLoader from "react-content-loader"
 const InvitationsLoader = (props) => (
   <ContentLoader 
   speed={1.2}
-  width={700}
+  width={1000}
   height={100}
-  viewBox="0 0 870 168"
+  viewBox="0 0 1350 140"
   backgroundColor="#e3e3e3"
   foregroundColor="#d1d1d1"
   {...props}
   >
-    <rect x="90" y="12" rx="3" ry="3" width="200" height="15" /> 
-    <rect x="90" y="36" rx="3" ry="3" width="215" height="10" /> 
-    <rect x="90" y="56" rx="3" ry="3" width="240" height="6" /> 
-    <circle cx="38" cy="38" r="38" /> 
-    <rect x="680" y="12" rx="3" ry="3" width="60" height="27" /> 
-    <rect x="750" y="12" rx="3" ry="3" width="60" height="27" />
+    <rect x="120" y="16" rx="3" ry="3" width="250" height="22" /> 
+    <rect x="120" y="48" rx="3" ry="3" width="265" height="15" /> 
+    <rect x="120" y="72" rx="3" ry="3" width="290" height="8" /> 
+    <circle cx="48" cy="48" r="48 " /> 
+    <rect x="1100" y="12" rx="3" ry="3" width="90" height="32" /> 
+    <rect x="1000" y="12" rx="3" ry="3" width="90" height="32" />
   </ContentLoader>
 )
 
